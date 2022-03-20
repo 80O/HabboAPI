f5525c00e5451efbb3e3898fd40b9fa9ae835e62
80O <48831268+80O@users.noreply.github.com>
2022-03-20 20:08:57 +0100

    Added GetAchievementsCategories to Achievements endpoint.

--- HabboAPI/Achievements/AchievementsEndpoints.cs
+++ HabboAPI/Achievements/AchievementsEndpoints.cs
@@ -6,6 +6,9 @@ public static class AchievementsEndpoints
 {
     public static Task<List<Achievement>?> GetAchievements(this HabboAPI api) => api.Get<List<Achievement>>("api/public/achievements");
 
+    public static Task<IEnumerable<string>> GetAchievementsCategories(this HabboAPI api) => api.GetAchievements().ContinueWith(t => t.Result?.Select(a => a.Data.Category).Distinct() ?? Enumerable.Empty<string>());
+
+    public static Task<Dictionary<string, List<Achievement>>> GetAchievementsGroupedByCategory(this HabboAPI api) => api.GetAchievements().ContinueWith(t => t.Result?.GroupBy(ach => ach.Data.Category).ToDictionary(g => g.Key, g => g.ToList()) ?? new());
     public static Task<List<AchievementProgress>?> GetUserAchievements(this HabboAPI api, UniqueUserId uuid) =>
         api.Get<List<AchievementProgress>>($"api/public/achievements/{uuid}");
 }
